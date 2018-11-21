5aa9aa8052cc6a09af1d0ca7b7649bc2ba6299e5
jsteruya <jorge@lenord.com.br>
2018-11-21 14:57:03 -0200

    ajustado produto

--- _pages/produto.php
+++ _pages/produto.php
@@ -9,7 +9,7 @@
 ?>
 
 <script type="text/javascript">
-        function refreshCarrinho()
+		function refreshCarrinho()
         {
             window.location.reload(true); 
         }
@@ -46,8 +46,9 @@
 					$(this).html($('#previewCart ul li').length);
 				});
 			}
-                        refreshCarrinho();
+			refreshCarrinho();
 		});
+		
 	}
 
 	function addWishList() {
@@ -94,7 +95,7 @@
 				<div class="box-img">
 					<?php if($dadosProduto['ImagemPlus1']) : ?>
 						<div class="box-zoom">
-							<img src="<?= $dadosProduto['ImagemPlus1'] ?>" data-zoom-image="<?= $dadosProduto['ImagemPlus1'] ?>" />
+							<img style="width:100%;max-height:100%;" src="<?= $dadosProduto['ImagemPlus1'] ?>" data-zoom-image="<?= $dadosProduto['ImagemPlus1'] ?>" />
 						</div>
 					<?php else : ?>
 						<div>
@@ -129,17 +130,22 @@
 					<?php endif; ?>
 				</div>
 			</div>
+			<!--
 			<div class="box-share">
 				<span class="title">Compartilhar</span>
-				<a href="javascript: void(0);" onclick="window.open('https://twitter.com/intent/tweet?text=Gostei+de+um+produto+da+Painful&url=<?= urlencode(URLSite . ltrim($URISite,"/")) ?>&hashtags=malinabeauty','twitter', 'toolbar=0, status=0, width=650, height=450');"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
 				<a href="javascript: void(0);" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=<?= urlencode(URLSite . ltrim($URISite,"/")) ?>','facebook', 'toolbar=0, status=0, width=650, height=450');"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+				<a href="javascript: void(0);" onclick="window.open('https://twitter.com/intent/tweet?text=Gostei+de+um+produto+da+Painful&url=<?= urlencode(URLSite . ltrim($URISite,"/")) ?>&hashtags=malinabeauty','twitter', 'toolbar=0, status=0, width=650, height=450');"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
 			</div>
+			-->
 		</div>
 
 		<!-- Product right -->
 		<div class="col-sm-6 prod-rt">
 			<form name="dadosProduto" id="dadosProduto" method="post" onsubmit="false">
-				<h4 class="title"><?= $dadosProduto['Descricao'] ?></h4>
+				<div class="lenord_produto_referencia" style="margin-bottom: 15px;">
+					<span><b>Referência:</b></span> (Colocar código ref.)
+				</div>
+				<h4 class="title" style="color:#22428e;"><?= $dadosProduto['Descricao'] ?></h4>
 				<div class="box-wish">
 					<?php if (!empty($dadosLogin['ID']) && $dadosLogin['ID'] > 0) : ?>
 						<a href="javascript:addWishList();" class="wish"><i class="fa fa-heart-o"></i> <span id="retornoWishlist">Adicionar à minha Wishlist</span></a>
@@ -283,6 +289,34 @@
 				<?php
 						$parcelamento = getRest(str_replace(['{IDProduto}', '{valorProduto}'], [$dadosProduto['ID'], $dadosProduto['PrecoVigente']], $endPoint['parcelamento']));
 				?>
+
+					<div class="lenord_frete" style="margin-top:15px;">
+						<div class="box-share">
+							<button style="padding:5px;"><a href="javascript: void(0);" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=<?= urlencode(URLSite . ltrim($URISite,"/")) ?>','facebook', 'toolbar=0, status=0, width=650, height=450');"><i class="fa fa-facebook-square" aria-hidden="true" style="#3B5998"></i></a>
+								Compartilhar
+							</button>
+
+							<button style="padding:5px;">
+								<a href="javascript: void(0);" onclick="window.open('https://twitter.com/intent/tweet?text=Gostei+de+um+produto+da+Painful&url=<?= urlencode(URLSite . ltrim($URISite,"/")) ?>&hashtags=malinabeauty','twitter', 'toolbar=0, status=0, width=650, height=450');"><i class="fa fa-twitter-square" aria-hidden="true" style="color:#2AAAE0;"></i></a>
+								Compartilhar
+							</button>
+						</div>
+					</div>
+
+					<div class="lenord_frete" style="margin-top:15px;">
+					 <div style="width:100%; padding:5px; color:#fff; background-color:#22428e">
+					 	<center>Cálculo de Frete</center>
+					 </div>	
+					 <div style="background-color: #eee; padding:5px;">
+					 	<span style="width:50%">
+						 <input type="text" placeholder="00000-000">
+						</span>
+						<span style="width:50%; margin-left: 10px;">
+						<button type="submit">Calcular</button>
+						</span>
+					 </div>
+					</div>
+
 					<div class="box-installment">
 						<?php
 						if (!empty($parcelamento[0]) && $parcelamento[0]['Numero'] === 0) {
